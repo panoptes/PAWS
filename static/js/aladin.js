576bd6b7d82938151e8219b8793ce426e856606f
gnthibault <gnthibault@gmail.com>
2021-02-14 16:59:55 +0100

    Aladin is working awesome

--- static/js/aladin.js
+++ static/js/aladin.js
@@ -10,6 +10,28 @@ function onAladinLoadHandler() {
   //var aladin_bil = aladin.getBaseImageLayer();
   //var aladin_initial_color_map = aladin_bil.getColorMap();
   //aladin_initial_color_map.update("rainbow");
+  updateAladin()
+}
+
+function updateAladin() {
+  results={};
+  jQuery.get('http://localhost:8000/static/images/HIPS/properties', function(data) {
+    parts=data.split("\n");
+    for (var i=0;i<parts.length;i++){
+      var nameAndValue=parts[i].split("=")
+      if(nameAndValue.length==2) {
+	    results[ $.trim(nameAndValue[0]) ] = $.trim(nameAndValue[1])
+	  }
+    }
+
+    dec = parseFloat(results["hips_initial_dec"].match(/-?(?:\d+(?:\.\d*)?|\.\d+)/)[0])
+    ra = parseFloat(results["hips_initial_ra"].match(/-?(?:\d+(?:\.\d*)?|\.\d+)/)[0])
+    fov = parseFloat(results["hips_initial_fov"].match(/-?(?:\d+(?:\.\d*)?|\.\d+)/)[0])
+
+    // Update position
+    Aladin.aladin.gotoRaDec(ra, dec)
+    Aladin.aladin.setFov(fov)
+  });
 }
 
 $( document ).keyup(function(e) {
